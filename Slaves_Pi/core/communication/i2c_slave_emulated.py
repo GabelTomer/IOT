b8ad650f7f72687d33a48e851378bc9a4bf2c0a1
Marvel-Witch <dor970@gmail.com>
2025-06-07 13:26:40 +0300

    add gpio interupt for communication

--- Slaves_Pi/core/communication/i2c_slave_emulated.py
+++ Slaves_Pi/core/communication/i2c_slave_emulated.py
@@ -4,12 +4,15 @@ import struct
 
 # Respond with the same 16-byte payload
 class SimpleI2CSlave:
-    def __init__(self, address):
+    def __init__(self, address, data_gpio):
         self.pi = pigpio.pi()
         if not self.pi.connected:
             raise Exception("Cannot connect to pigpiod")
         self.handle = self.pi.bsc_i2c(address)
         self.address = address
+        self.data_gpio = data_gpio
+        self.pi.set_mode(data_gpio, pigpio.OUTPUT)
+        self.pi.write(data_gpio, 0)
         print(f"I2C slave ready on address 0x{address:X}")
 
     def listen_and_respond(self, data_bytes):
